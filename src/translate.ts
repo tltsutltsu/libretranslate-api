c9724d70d40480337b96d99dbcd890757e7894de
Elijah <83283675+tltsutltsu@users.noreply.github.com>
2022-07-24 11:05:07 +0600

    Faster, but less safer realisation

--- src/translate.ts
+++ src/translate.ts
@@ -3,46 +3,29 @@ interface TranslateOptions {
     source?: string;
     target: string;
     format?: string;
-	apiurl?: string
+    apiUrl?: string
     apiKey?: string;
 }
 
-export const translate = async({ query, source, target, format, apiurl, apiKey }: TranslateOptions) => {
-    if (!apiurl && !apiKey) throw new TypeError("You need an API key to use the public LibreTranslate API!");
-	if (!apiurl) apiurl = "https://libretranslate.com";
-	if (!source) source = "auto";
+export const translate = async({ query, source, target, format, apiUrl, apiKey }: TranslateOptions) => {
+    if (!apiUrl && !apiKey) throw new TypeError("You need an API key to use the public LibreTranslate API!");
+    if (!apiUrl) apiUrl = "https://libretranslate.com";
+    if (!source) source = "auto";
 
-	let availableLanguages: string[] = [];
-	availableLanguages.push("auto");
-
-	const res1 = await fetch(`${apiurl}/languages`, {
-		method: "GET",
-		headers: { "Content-Type": "application/json" }
-	})
-	
-	const languages = await res1.json();
-	languages.forEach(language => {
-		availableLanguages.push(language.code);
-	});
-	
-	
-	if (!availableLanguages.includes(source)) throw new TypeError("This language was not found/does not exist.");
-	if (!availableLanguages.includes(target)) throw new TypeError("This language was not found/does not exist.");
-
-	const res2 = await fetch(`${apiurl}/translate`, {
+    const res = await fetch(`${apiUrl}/translate`, {
 	method: "POST",
 	body: JSON.stringify({
-		q: query,
-		source: source,
-		target: target,
-		format: format || "text",
-		apiKey: apiKey || "",
+	    q: query,
+	    source: source,
+	    target: target,
+	    format: format || "text",
+	    apiKey: apiKey || "",
 	}),
 	headers: { "Content-Type": "application/json" }
 });
 
   try {
-	let t = await res2.json();
+	let t = await res.json();
 	return t.translatedText;
   } catch (err) {
 	throw err;
